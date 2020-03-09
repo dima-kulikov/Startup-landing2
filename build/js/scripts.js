dd049071882eb50469c7a8a97ecb8c590294e84c
dima-kulikov <53553661+dima-kulikov@users.noreply.github.com>
2020-03-09 23:50:46 +0200

    2120
    51

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -34,10 +34,21 @@ $(function () {
 // });
 "use strict";
 
-(function ($) {
-  $(function () {
-    $('.menu__icon').on('click', function () {
-      $(this).closest('.menu').toggleClass('menu_state_open');
-    });
+$(function () {
+  $('.menu__icon').on('click', function () {
+    $(this).closest('.menu').toggleClass('menu_state_open');
   });
-})(jQuery);
+  $('.menu__links-item').on('click', function (e) {
+    $(this).closest('.menu').removeClass('menu_state_open');
+    var url = this.href.split('#')[1];
+    scrollToAnchor(url);
+  });
+});
+jQuery;
+
+function scrollToAnchor(aid) {
+  var aTag = $("a[name='" + aid + "']");
+  $('html,body').animate({
+    scrollTop: aTag.offset().top
+  }, 'slow');
+}
--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,7 +1,24 @@
-(function($){
-    $(function() {
-      $('.menu__icon').on('click', function() {
-        $(this).closest('.menu').toggleClass('menu_state_open');
-      });
+
+
+  $(function() {
+    $('.menu__icon').on('click', function() {
+      $(this)
+        .closest('.menu')
+        .toggleClass('menu_state_open');
     });
-  })(jQuery);
+    $('.menu__links-item').on('click', function(e) {
+      $(this)
+        .closest('.menu')
+        .removeClass('menu_state_open');
+
+      const url = this.href.split('#')[1];
+
+      scrollToAnchor(url);
+    });
+  });
+(jQuery);
+
+function scrollToAnchor(aid) {
+  const aTag = $("a[name='" + aid + "']");
+  $('html,body').animate({ scrollTop: aTag.offset().top }, 'slow');
+}
